fea36b5c5dc871540c36b350100932fd0b65c483
Eric Groom <eric@gingerlabs.com>
2022-12-07 15:04:56 -0800

    wipe transition

--- HackathonPresentation/HackathonPresentation/Slides.swift
+++ HackathonPresentation/HackathonPresentation/Slides.swift
@@ -11,18 +11,25 @@ struct Slides: View {
     @State var slideCount = 0
     @Namespace var presentationNamespace
     @Environment(\.presentationScale) var scale
+    private let left = 0.15
+    private let right = 0.65
     
     var body: some View {
         ZStack {
-            Color.white
-            TrapezoidThing(leftHeightPercentage: slideCount == 0 ? 0.15 : 1.0, rightHeightPercentage: slideCount == 0 ? 0.65 : 1.0)
-                .fill(slideCount == 0 ? Color("GoogleSlidesBackground") : Color("PalenightBackground"))
             ZStack {
-                if slideCount == 0 {
-                    TrollTitleSlide(namespace: presentationNamespace)
-                } else {
-                    TitleSlide(namespace: presentationNamespace)
-                }
+                TrollTitleSlide(namespace: presentationNamespace)
+                    .background(.white)
+                    .mask(
+                        TrapezoidThing(leftHeightPercentage: slideCount == 0 ? 1 - right : 0.0, rightHeightPercentage: slideCount == 0 ? 1 - left : 0.0)
+                            .scale(x: -1, y: -1, anchor: .center)
+                            .fill(.white)
+                    )
+                TitleSlide(namespace: presentationNamespace)
+                    .background(slideCount == 0 ? Color("GoogleSlidesBackground") : Color("PalenightBackground"))
+                    .mask(
+                        TrapezoidThing(leftHeightPercentage: slideCount == 0 ? left : 1.0, rightHeightPercentage: slideCount == 0 ? right : 1.0)
+                .fill(.white)
+                    )
                 Group {
                     Button {
                         withAnimation {
